297353558c644e95a534f0ad156ee0bf39596e8f
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 09:50:37 -0500

    Update GameDriver.java

--- AnenomeEnemy/GameDriver.java
+++ AnenomeEnemy/GameDriver.java
@@ -11,10 +11,17 @@ public class GameDriver {
         // new Slime(...)
         // new Skeleton(...)
         // new Dragon(...)
+        Enemy[] enemies = {new Slime(5,1,"Bob"), new Skeleton(10,5,"Jack"), new Dragon(50,20,"Toothless")};
 
         // TODO: Loop through the array and call:
         // update(), attack(), takeDamage()
+        for (int i = 0; i < enemies.length; i++) {
+            enemies[i].update();
+            enemies[i].attack();
+            enemies[i].takeDamage(5);
+        }
 
         // TODO: Add reflection answers as comments at the bottom of this file
+        // in the readme
     }
 }
