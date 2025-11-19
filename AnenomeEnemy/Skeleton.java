740e84cbbd37b50008582ca2b95fb7749669fc19
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 10:10:40 -0500

    Update Skeleton.java

--- AnenomeEnemy/Skeleton.java
+++ AnenomeEnemy/Skeleton.java
@@ -14,6 +14,7 @@ public class Skeleton extends Enemy {
     // TODO: Implement update()
     @Override
     void update(){
+        System.out.println("--------|UPDATE|--------");
         System.out.println(this.name + " skeleton marches forward");
     }
 
@@ -21,6 +22,7 @@ public class Skeleton extends Enemy {
     // TODO: Implement attack()
     @Override
     void attack(){
+        System.out.println("--------|ATTACK|--------");
         System.out.println(this.name + " throws it's bones at you!");
         System.out.println("The attack does " + this.damage + " damage! :0");
     }
