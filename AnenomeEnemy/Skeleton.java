ca0557c2e94a659115cba657ca509afd09b4fc55
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 09:41:27 -0500

    Update Skeleton.java

--- AnenomeEnemy/Skeleton.java
+++ AnenomeEnemy/Skeleton.java
@@ -2,12 +2,25 @@
 
 // TODO: Extend Enemy
 
-public class Skeleton {
+public class Skeleton extends Enemy {
 
     // TODO: Create constructor (call super)
+    public Skeleton(int health, int damage, String name){
+        super(health,damage,name);
+    }
 
     // TODO: Implement update()
+    @Override
+    void update(){
+        System.out.println("The skeleton marches forward");
+    }
+
 
     // TODO: Implement attack()
+    @Override
+    void attack(){
+        System.out.println("The skeleton throws it's bones at you!");
+        System.out.println("The attack does " + this.damage + " damage! :0");
+    }
 }
 
