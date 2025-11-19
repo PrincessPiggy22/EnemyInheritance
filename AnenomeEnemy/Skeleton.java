e93921a5f009a532143fd234b1baf0f6932d6d5d
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 09:07:06 -0500

    Create Skeleton.java

---
+++ AnenomeEnemy/Skeleton.java
@@ -0,0 +1,13 @@
+// Skeleton enemy subclass
+
+// TODO: Extend Enemy
+
+public class Skeleton {
+
+    // TODO: Create constructor (call super)
+
+    // TODO: Implement update()
+
+    // TODO: Implement attack()
+}
+
