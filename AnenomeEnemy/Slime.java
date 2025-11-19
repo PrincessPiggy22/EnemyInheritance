713f2486375113328af812c389a194b465b8dcd3
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 09:36:30 -0500

    Update Slime.java

--- AnenomeEnemy/Slime.java
+++ AnenomeEnemy/Slime.java
@@ -2,12 +2,27 @@
 
 // TODO: Extend Enemy
 
-public class Slime {
+public class Slime extends Enemy{
 
     // TODO: Create constructor
     // Must call super(...)
+    public Slime(){
+        super(health,damage,name);
+        this.health = health;
+        this.damage = damage;
+        this.name = name;
+    }
 
     // TODO: Implement update()
+    @Override
+    void update(){
+        System.out.println("The slime slugs forward");
+    }
 
     // TODO: Implement attack()
+    @Override
+    void attack(){
+        System.out.println("The slime slimes you!");
+        System.out.println("The attack does " + this.damage + " damage! :0");
+    }
 }
