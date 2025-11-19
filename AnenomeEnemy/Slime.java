3a99d18d0d9e525ba5579c2da0d11e2788372852
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 10:04:20 -0500

    added output formatting to Slime.java

--- AnenomeEnemy/Slime.java
+++ AnenomeEnemy/Slime.java
@@ -2,24 +2,31 @@
 
 // TODO: Extend Enemy
 
+
 public class Slime extends Enemy{
 
     // TODO: Create constructor
     // Must call super(...)
     public Slime(int health, int damage, String name){
         super(health,damage,name);
+        System.out.println("BOSS BATTLE ALERT\n-----------------");
+        System.out.println(this.name + " THE SLIME\n");
     }
 
     // TODO: Implement update()
     @Override
     void update(){
+        System.out.println("--------|UPDATE|--------");
         System.out.println("The slime slugs forward");
+        //System.out.println("\n");
     }
 
     // TODO: Implement attack()
     @Override
     void attack(){
+        System.out.println("--------|ATTACK|--------");
         System.out.println("The slime slimes you!");
         System.out.println("The attack does " + this.damage + " damage! :0");
+        //System.out.println("\n");
     }
 }
