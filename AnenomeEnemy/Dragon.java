ea95204b7da682322964540a9629d8ebe8109a03
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 10:09:36 -0500

    Update Dragon.java

--- AnenomeEnemy/Dragon.java
+++ AnenomeEnemy/Dragon.java
@@ -7,18 +7,20 @@ public class Dragon extends Enemy {
     // TODO: Create constructor (call super)
     public Dragon(int health, int damage, String name){
         super(health,damage,name);
+        System.out.println("BOSS BATTLE ALERT\n-----------------");
+        System.out.println(this.name + " THE DRAGON\n");
     }
-    
+
     // TODO: Implement update()
     @Override
     void update(){
-        System.out.println("The dragon stomps forward");
+        System.out.println(this.name + " stomps forward");
     }
-    
+
     // TODO: Implement attack()
     @Override
     void attack(){
-        System.out.println("The dragon blasts fire at you!");
+        System.out.println(this.name +  "blasts fire at you!");
         System.out.println("The attack does " + this.damage + " damage! :0");
     }
 }
