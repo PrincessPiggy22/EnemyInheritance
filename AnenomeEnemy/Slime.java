8735b5d483a0fcaa2f90535df24f702998f0c47b
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 09:41:04 -0500

    Update Slime.java

--- AnenomeEnemy/Slime.java
+++ AnenomeEnemy/Slime.java
@@ -6,11 +6,8 @@ public class Slime extends Enemy{
 
     // TODO: Create constructor
     // Must call super(...)
-    public Slime(){
+    public Slime(int health, int damage, String name){
         super(health,damage,name);
-        this.health = health;
-        this.damage = damage;
-        this.name = name;
     }
 
     // TODO: Implement update()
