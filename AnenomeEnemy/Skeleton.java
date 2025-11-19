271456583c0bf1d98d0e9e41e8d74270c427671e
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-19 10:07:28 -0500

    added output formatting to Skeleton.java

--- AnenomeEnemy/Skeleton.java
+++ AnenomeEnemy/Skeleton.java
@@ -7,19 +7,21 @@ public class Skeleton extends Enemy {
     // TODO: Create constructor (call super)
     public Skeleton(int health, int damage, String name){
         super(health,damage,name);
+        System.out.println("BOSS BATTLE ALERT\n-----------------");
+        System.out.println(this.name + " SKELETON\n");
     }
 
     // TODO: Implement update()
     @Override
     void update(){
-        System.out.println("The skeleton marches forward");
+        System.out.println(this.name + " skeleton marches forward");
     }
 
 
     // TODO: Implement attack()
     @Override
     void attack(){
-        System.out.println("The skeleton throws it's bones at you!");
+        System.out.println(this.name + " throws it's bones at you!");
         System.out.println("The attack does " + this.damage + " damage! :0");
     }
 }
